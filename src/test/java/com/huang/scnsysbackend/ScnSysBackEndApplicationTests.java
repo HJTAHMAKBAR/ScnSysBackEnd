fcd02b529ebda74549b1b7ef599b8af20dd59038
hjtahmakbar <hjtahmakbar@gmail.com>
2023-03-14 15:45:47 +0800

    first commit

---
+++ src/test/java/com/huang/scnsysbackend/ScnSysBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.scnsysbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScnSysBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
