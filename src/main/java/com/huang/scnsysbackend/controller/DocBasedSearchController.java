d8634037221d88850d54a509c7d89948fd225b28
Jett Huang <hjtahmakbar@gmail.com>
2023-04-13 23:47:48 +0800

    添加跨域

--- src/main/java/com/huang/scnsysbackend/controller/DocBasedSearchController.java
+++ src/main/java/com/huang/scnsysbackend/controller/DocBasedSearchController.java
@@ -6,6 +6,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 
 @RestController
@@ -13,8 +15,13 @@ import java.io.FileNotFoundException;
 public class DocBasedSearchController {
 
     @PostMapping("/upload")
-    public RespBean uploadSingleDocFile(@RequestParam(value = "singleDoc") MultipartFile file, HttpServletRequest request) throws FileNotFoundException {
-        System.out.printf(file.toString());
+    public RespBean uploadSingleDocFile(@RequestParam(value = "singleDoc") MultipartFile file) throws IOException {
+        String filename = file.getOriginalFilename();
+        if (filename.endsWith(".pdf")) {
+            Scanner scanner = new Scanner(file.getInputStream());
+            System.out.println(scanner.nextLine());
+        }
+        System.out.println("你好");
         return RespBean.success("ok");
     }
 
